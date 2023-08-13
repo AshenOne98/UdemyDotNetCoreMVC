737444abb5091052f04c86a5a0440c6617bdf60d
Arif <muhdarif9805@gmail.com>
2023-08-13 16:18:08 +0800

    Add category page & seed for category

--- UdemyDotNetCoreMVC/Migrations/20230813081640_SeedCategoryTable.Designer.cs
+++ UdemyDotNetCoreMVC/Migrations/20230813081640_SeedCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UdemyDotNetCoreMVC.Data;
 
@@ -10,9 +11,11 @@ using UdemyDotNetCoreMVC.Data;
 namespace UdemyDotNetCoreMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230813081640_SeedCategoryTable")]
+    partial class SeedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
